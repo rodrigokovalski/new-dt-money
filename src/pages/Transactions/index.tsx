f612a92e832906873e6f34b68a7e1087d4aa114d
rodrigokovalski <rodrigokvk@gmail.com>
2025-03-14 03:42:00 -0300

    feat[estrutura-visual]: Criando componente visual de sumário. Biblioteca de ícones phosphor-react

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/header";
+import { Summary } from "../../components/Summary";
 
 export function Transactions() {
     return (
         <div>
             <Header />
+            <Summary />
         </div>
     )
 };
