b0decc463c3305118f659846520708784a9373b0
rodrigokovalski <rodrigokvk@gmail.com>
2025-03-14 21:57:57 -0300

    fix: ajustando utilização de useMemo

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -11,25 +11,26 @@ export function useSummary() {
 
     // transactions[]  ==> incode: 0 , outcome: 0, total: 0;
     const summary = useMemo(() => {
-        transactions.reduce(
+        return transactions.reduce(
             (acc, transaction) => {
                 if (transaction.type === 'income') {
                     acc.income += transaction.price
-                    acc.total += transaction.price;
+                    acc.total += transaction.price
                 } else {
-                    acc.outcome += transaction.price;
-                    acc.total -= transaction.price;
+                    acc.outcome += transaction.price
+                    acc.total -= transaction.price
                 }
 
-                return acc;
+                return acc
             },
             {
                 income: 0,
                 outcome: 0,
-                total: 0
-            }
-        );
-    }, [transactions]);
+                total: 0,
+            },
+        )
+    }, [transactions])
+
 
     return summary;
 }
